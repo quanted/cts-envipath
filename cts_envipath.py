992fec76da21fb119642f019bf44281c62dd3d2e
kurtw555 <kurt.wolfe@gmail.com>
2021-01-05 17:34:53 -0500

    Add a bit of exception handling and logging

--- cts_envipath.py
+++ cts_envipath.py
@@ -1,5 +1,7 @@
 import json
 import time
+import sys
+import logging
 import pandas as pd
 import requests
 from pprint import pprint
@@ -16,59 +18,66 @@ class CTSEnvipath:
         self.package_id = INSTANCE_HOST + 'package/' + '650babc9-9d68-4b73-9332-11972ca26f7b'
 
     def get_envipath_tree(self, smiles):
-        ep = enviPath(INSTANCE_HOST)
+        try:
 
-        # Create package object
-        p = Package(ep.requester, id=self.package_id)
-        print("calling predict")
-        pw = p.predict(smiles, name='Pathway via REST', description='A pathway created via REST')
-        print("finished calling predict")
+            ep = enviPath(INSTANCE_HOST)
 
-        json_retval = pw.get_json()
-        idx = 0
-        # Loop until completed flag switches
-        while json_retval['completed'] == 'false':
-            # Sleep for 10 seconds
-            idx = idx + 1
-            print("step: " + str(idx))
-            time.sleep(10)
-            json_retval = pw.get_json()                
-        
-        nodes = json_retval['nodes']
-        links = json_retval['links']
+            # Create package object
+            p = Package(ep.requester, id=self.package_id)
+            print("calling predict")
+            pw = p.predict(smiles, name='Pathway via REST', description='A pathway created via REST')
+            print("finished calling predict")
 
-        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        for link in links:
-            if link['pseudo'] == False:
-                idreaction = link['idreaction']
-                response = requests.get(idreaction, headers=headers)
-                reaction = response.text
-                                
-                reaction = response.json()
-                rules = reaction['rules']
-                rule = rules[0]['name']
-                link["rule"] = rule         
-                #with open("link" + ".json", "w") as text_file:
-                #    text_file.write(json.dumps(link)) 
+            json_retval = pw.get_json()
+            idx = 0
+            # Loop until completed flag switches
+            while json_retval['completed'] == 'false':
+                # Sleep for 10 seconds
+                idx = idx + 1
+                print("step: " + str(idx))
+                time.sleep(10)
+                json_retval = pw.get_json()                
+            
+            nodes = json_retval['nodes']
+            links = json_retval['links']
 
-        retval = json.dumps(json_retval)
-        envipath_data = retval.replace("'", '"')
-        pprint(envipath_data)
+            headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+            for link in links:
+                if link['pseudo'] == False:
+                    idreaction = link['idreaction']
+                    response = requests.get(idreaction, headers=headers)
+                    reaction = response.text
+                                    
+                    reaction = response.json()
+                    rules = reaction['rules']
+                    rule = rules[0]['name']
+                    link["rule"] = rule         
+                    #with open("link" + ".json", "w") as text_file:
+                    #    text_file.write(json.dumps(link)) 
 
-        with open(smiles + ".json", "w") as text_file:
-            text_file.write(retval)
+            retval = json.dumps(json_retval)
+            envipath_data = retval.replace("'", '"')
+            #pprint(envipath_data)
 
-        # Load dataframe of eawag rules called "paths"
-        df_paths = pd.read_pickle('paths.pkl')
+            #with open(smiles + ".json", "w") as text_file:
+            #    text_file.write(retval)
 
-        cts_envipath_tree = Tree(nodes, links, df_paths)
-        cts_envipath_tree.build_tree()
-        return cts_envipath_tree
+            # Load dataframe of eawag rules called "paths"
+            df_paths = pd.read_pickle('paths.pkl')
+
+            cts_envipath_tree = Tree(nodes, links, df_paths)
+            cts_envipath_tree.build_tree()
+            
+
+        except Exception as e:
+            msg = e.args[0]
+            logging.warning(msg)
+            cts_envipath_tree = msg
+
+        finally:
+            return cts_envipath_tree
         
 if __name__ == "__main__":
 
     ctsenvipath = CTSEnvipath()
-    tree = ctsenvipath.get_envipath_tree('c1ccccc1')
-    ival = 1
-    #ctsenvipath.get_envipath_tree('O=C(C)Oc1ccccc1C(=O)O')
-    #ctsenvipath.get_envipath_tree('Cc1ccccc1')
+    tree = ctsenvipath.get_envipath_tree('c1ccccc1')    
